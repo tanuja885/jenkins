e309871a8af3ce474dfd793182c7ed2513372f42
HP <HP@DESKTOP-E47BSIE>
2024-07-05 15:15:42 +0530

    jenkins

---
+++ jenkins/src/main/java/jenkins/Launch.java
@@ -0,0 +1,16 @@
+package jenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Launch {
+
+	@Test
+	public void login() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+
+	}
+}
